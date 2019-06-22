279fc3c1eae5e51158e30783d8f0f317fd1e375b
丶景凉夜雨 <zs1061674782@gmail.com>
2019-06-22 19:25:36 +0800

    springboot入门

---
+++ springboot-begin/src/main/java/com/zhouson/MySpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.zhouson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouson
+ * @create 2019-06-22 19:10
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
---
+++ springboot-begin/src/main/java/com/zhouson/controller/QuickController.java
@@ -0,0 +1,19 @@
+package com.zhouson.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author zhouson
+ * @create 2019-06-22 19:14
+ */
+@Controller
+public class QuickController {
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+     return "hello springboot";
+    }
+}
