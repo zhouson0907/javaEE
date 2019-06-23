9e3d8866d0ee9985e4f1a1d2a9399aeea706e0cd
丶景凉夜雨 <zs1061674782@gmail.com>
2019-06-23 11:06:42 +0800

    springboot入门

---
+++ springboot-fast/src/main/java/com/zhouson/springbootfast/SpringbootFastApplication.java
@@ -0,0 +1,13 @@
+package com.zhouson.springbootfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFastApplication.class, args);
+    }
+
+}
