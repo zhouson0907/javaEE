49050d003ea6f6adc84e05fd4dac8fa214f9e48b
丶景凉夜雨 <zs1061674782@gmail.com>
2019-06-23 11:37:38 +0800

    springboot入门

---
+++ springboot-fast/src/main/java/com/zhouson/springbootfast/controller/controller.java
@@ -0,0 +1,22 @@
+package com.zhouson.springbootfast.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author zhouson
+ * @create 2019-06-23 11:34
+ */
+@Controller
+public class controller {
+    //获取配置文件中的"name"信息
+    @Value("${name}")
+    private String name;
+    @RequestMapping("/quick2")
+    @ResponseBody
+    public String quick2(){
+        return "name:"+name;
+    }
+}
